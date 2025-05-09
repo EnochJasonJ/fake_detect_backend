4c6fef2db7b5aacd00fe3c2c89e09bda1f93e9c4
EnochJasonJ <enochjason06@gmail.com>
2025-05-09 23:58:17 +0530

    Changed debug mode

--- project/settings.py
+++ project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_1@vml661!y-8khhv-q*n$-k6)l%++kq8nr0x))cmem)40!@xg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['https://fakedetectbackend-production.up.railway.app']
